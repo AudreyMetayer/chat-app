a79211d1a345e8185212d42342d0d5c01f1fc8e0
mtyerdua <58461935+mtyerdua@users.noreply.github.com>
2021-01-18 22:12:37 +0100

    Quest 3

--- src/App.js
+++ src/App.js
@@ -3,25 +3,12 @@ import React from 'react';
 import './App.css';
 import './components/Contact';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
       <div className="App">
-          <Contact
-              name="Lana Kane"
-              avatar="https://i.postimg.cc/WpY1dG61/lana.jpg"
-              onlineStatus="online"
-          />
-          <Contact
-              name="Cyril Figgis"
-              avatar="https://i.postimg.cc/nrz6DwfV/cyril.jpg"
-              onlineStatus=""
-          />
-          <Contact
-              name="Sterling Archer"
-              avatar="https://i.postimg.cc/MHkLYg8G/archer.jpg"
-              onlineStatus="online"
-          />
+   <ContactList />
       </div>
   );
 }
